663bc024fd1e6c4b1f39ba6070d5f7bfcd4f9a96
Jonathan Lister <jonathanl2013@hotmail.com>
2024-01-24 23:13:29 -0800

    fixed lsp.lua and treesitter.lua

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Neovim Setup and configs
+
+Currently still fixing the LSP Server because I was following Primagen Guide but it is not up to date.
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "rust", "lua", "c", "cpp", "python" },
+  ensure_installed = { "javascript", "typescript", "rust", "lua", "c_sharp", "c", "cpp", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
