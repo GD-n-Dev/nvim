d8b61e70b94e69ec4338205a4fb81866360a3e46
Jonathan Lister <jlister@inland-engineering.com>
2024-02-29 17:18:24 -0800

    trying to add snippets

--- lua/jlister/remap.lua
+++ lua/jlister/remap.lua
@@ -2,5 +2,16 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<Leader>pv', vim.cmd.Ex)
 
+-- Harpoon 'File Switching'
 vim.keymap.set('n', '<C-a>', ":lua require('harpoon.mark').add_file()<CR>")
 vim.keymap.set('n', '<C-e>', ":lua require('harpoon.ui').toggle_quick_menu()<CR>")
+
+-- Snippets to make life easier
+vim.keymap.set('n', '<Leader>fn',
+  function()
+    if vim.bo.filetype == "lua" then
+      return '0ifunction()^Mend<Esc>'
+    end
+  end
+)
+
