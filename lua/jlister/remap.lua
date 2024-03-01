ab558ece339c7f66fa048cd9eb52c1f378f44bee
Jonathan Lister <jlister@inland-engineering.com>
2024-02-29 16:02:14 -0800

    remapped some harpoon keymaps

--- lua/jlister/remap.lua
+++ lua/jlister/remap.lua
@@ -2,4 +2,5 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<Leader>pv', vim.cmd.Ex)
 
--- vim.keymap.set('n', '<C-a>', vim.cmd(require("harpoon.mark").add_file()))
+vim.keymap.set('n', '<C-a>', ":lua require('harpoon.mark').add_file()<CR>")
+vim.keymap.set('n', '<C-e>', ":lua require('harpoon.ui').toggle_quick_menu()<CR>")
