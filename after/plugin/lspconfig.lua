7613e8fb4d9435d2832cc11cd54bc6f0cec08c2e
Jonathan Lister <jlister@inland-engineering.com>
2024-12-09 09:58:26 -0800

    Trying to fix treesitter and LSP

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -2,7 +2,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-  ensure_installed = {'lua_ls', 'bashls', 'cssls', 'dockerls', 'html', 'ts_ls', 'jsonls', 'remark_ls', 'pylyzer', 'volar', 'yamlls'},
+  ensure_installed = {'lua_ls', 'bashls', 'cssls', 'dockerls', 'html', 'ts_ls', 'jsonls', 'pylyzer', 'volar', 'yamlls'},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
