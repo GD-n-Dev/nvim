ea08118b19d353f081af90473b0b61be52c08394
Jonathan Lister <jlister@inland-engineering.com>
2025-01-07 10:17:47 -0800

    added cmp_nvim for autocompletion to work

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -19,6 +19,32 @@ lspconfig_defaults.capabilities = vim.tbl_deep_extend(
     require('cmp_nvim_lsp').default_capabilities()
 )
 
+local cmp = require('cmp')
+cmp.setup({
+    window = {
+        completion    = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        {
+            name = 'buffer',
+            option = {
+                get_buffers = function()
+                    return vim.api.nvim_list_bufs()
+                end
+            }
+        }
+    })
+})
+
 
 vim.api.nvim_create_autocmd('LspAttach', {
     desc = 'LSP actions',
