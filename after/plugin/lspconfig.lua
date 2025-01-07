b0f7c01be51a3db249eade6829377b8c403b1d24
Jonathan Lister <jlister@inland-engineering.com>
2025-01-07 09:59:52 -0800

    added marksman for lsp markdown

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -4,7 +4,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
     -- Replace the language servers listed here
     -- with the ones you want to install
-    ensure_installed = { 'ols', 'lua_ls', 'bashls', 'cssls', 'dockerls', 'html', 'ts_ls', 'jsonls', 'pylyzer' },
+    ensure_installed = { 'ols', 'lua_ls', 'bashls', 'cssls', 'marksman', 'dockerls', 'html', 'ts_ls', 'jsonls', 'pylyzer' },
     handlers = {
         function(server_name)
             require('lspconfig')[server_name].setup({})
