8de1d7dba18e9dc09e0ef3a8019f79881b39ce7a
Jonathan Lister <jlister@inland-engineering.com>
2025-01-07 10:23:02 -0800

    Finished adding cmp plugs to get fully featured autocomplete

--- lua/vimplug.lua
+++ lua/vimplug.lua
@@ -3,41 +3,6 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
--- Shorthand notation for GitHub; translates to https://github.com/junegunn/seoul256.vim.git
--- Plug('junegunn/seoul256.vim')
-
--- Any valid git URL is allowed
--- Plug('https://github.com/junegunn/vim-easy-align.git')
-
--- Using a tagged release; wildcard allowed (requires git 1.9.2 or above)
--- Plug('fatih/vim-go', { ['tag'] = '*' })
-
--- Using a non-default branch
--- Plug('neoclide/coc.nvim', { ['branch'] = 'release' })
-
--- Use 'dir' option to install plugin in a non-default directory
--- Plug('junegunn/fzf', { ['dir'] = '~/.fzf' })
-
--- Post-update hook: run a shell command after installing or updating the plugin
--- Plug('junegunn/fzf', { ['dir'] = '~/.fzf', ['do'] = './install --all' })
-
--- Post-update hook can be a lambda expression
--- Plug('junegunn/fzf', { ['do'] = function()
-  -- vim.fn['fzf#install']()
--- end })
-
--- If the vim plugin is in a subdirectory, use 'rtp' option to specify its path
--- Plug('nsf/gocode', { ['rtp'] = 'vim' })
-
--- On-demand loading: loaded when the specified command is executed
--- Plug('preservim/nerdtree', { ['on'] = 'NERDTreeToggle' })
-
--- On-demand loading: loaded when a file with a specific file type is opened
--- Plug('tpope/vim-fireplace', { ['for'] = 'clojure' })
-
--- Unmanaged plugin (manually installed and updated)
--- Plug('~/my-prototype-plugin')
-
 -- Place my Plugins below
 
 
@@ -49,8 +14,12 @@ Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.x' })
 
 -- LSP Config
 Plug("neovim/nvim-lspconfig")
-Plug("hrsh7th/nvim-cmp")
 Plug("hrsh7th/cmp-nvim-lsp")
+Plug("hrsh7th/cmp-buffer")
+Plug("hrsh7th/cmp-path")
+Plug("hrsh7th/cmp-cmdline")
+Plug("hrsh7th/nvim-cmp")
+-- AUTO Install LSP servers
 Plug("williamboman/mason.nvim")
 Plug("williamboman/mason-lspconfig.nvim")
 
