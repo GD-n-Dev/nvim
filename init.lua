79633053c5d43f788b1629ee447244437a262aca
Jonathan Lister <jonathanl2013@hotmail.com>
2025-08-18 19:15:21 -0700

    Got transparency working properly

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 require('configs/keymaps')
 require('configs/options')
 require('configs/packman')
+
+-- Background color
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -38,10 +38,3 @@ vim.g.netrw_banner = 0
 
 vim.o.background = "dark"
 
--- Background color
-vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-]]
